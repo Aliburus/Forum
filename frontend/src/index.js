3bd9add18aef1822473cd7f11760ff9fc0c7d9b5
Aliburus <aliburus1905@gmail.com>
2025-05-12 20:59:56 +0300

    Remove frontend as submodule

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css"; // <<< burada import edildiğine emin ol
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
--- frontend/src/services/profilServices.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API = `${process.env.REACT_APP_API_URL}/profil`;
-
-export const getProfil = (id) => axios.get(`${API}/${id}`);
-export const createProfil = (data) => axios.post(API, data);
-export const updateProfil = (id, data) => axios.put(`${API}/${id}`, data);
-export const deleteProfil = (id) => axios.delete(`${API}/${id}`);
